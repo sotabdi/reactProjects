4e5315544396f9912c71443dbb338e4bf93d28a9
Sotabdi <mhsotabdi@gmail.com>
2024-10-21 23:14:55 +0600

    upload user photo

--- chatApp/src/Components/ChatComponents/ChatRight/ChatRight.jsx
+++ chatApp/src/Components/ChatComponents/ChatRight/ChatRight.jsx
@@ -7,12 +7,25 @@ import { RiSendPlaneFill } from "react-icons/ri";
 import { useSelector } from "react-redux";
 import EmojiPicker from "emoji-picker-react";
 import ModalComponent from "../../CommonComponents/ModalComponents/ModalComponent";
+import {
+  getDownloadURL,
+  getStorage,
+  ref as imgSorageRef,
+  uploadBytesResumable,
+} from "firebase/storage";
+import { errorToast } from "../../../../Utils/Toast";
+import { getAuth } from "firebase/auth";
+import { getTime } from "../../../../Utils/MomentJs/Moment";
 const ChatRight = () => {
+  const auth = getAuth()
+  const storage = getStorage();
   const [emojiPicker, setemojiPicker] = useState(false);
   const { friends } = useSelector((state) => state.friends);
   const [massage, setmassage] = useState("");
   const [modalIsOpen, setIsOpen] = useState(false);
-  const [img , setimg] = useState('');
+  const [img, setimg] = useState(null);
+  const [imgdownloadURL, setimgdownloadURL] = useState(null);
+  const [progress, setprogress] = useState(null);
   const handleEmojiBtn = () => {
     setemojiPicker(!emojiPicker);
   };
@@ -28,9 +41,63 @@ const ChatRight = () => {
 
   function closeModal() {
     setIsOpen(false);
+    setimg(null);
+    setprogress(null)
   }
-const handleImage = (event)=>{
-  setimg(event.target.files[0]);
+
+  const handleImage = (event) => {
+    setimg(event.target.files[0]);
+  };
+console.log(friends);
+
+  // upload imaage and get download URL
+  const handleSentImg = () => {
+    let typeArr = ["image/png", "image/gif", "image/jpeg", "image/webp"];
+    if (!typeArr.includes(img.type)) {
+      errorToast(
+        'Flie Type must be "image/png", "image/gif", "image/jpeg", "image/webp"'
+      );
+    }
+    const storageRef = imgSorageRef(storage, `msgImg/${img.name}`);
+    const uploadTask = uploadBytesResumable(storageRef, img);
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        // Observe state change events such as progress, pause, and resume
+        // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        setprogress(progress);
+      },
+      (error) => {
+        errorToast(error.message);
+      },
+      () => {
+        // Handle successful uploads on complete
+        // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+        setprogress(null);
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          setimgdownloadURL(downloadURL);
+          
+        })
+      }
+    );
+  };
+
+const handleSendMsg = () => {
+  let obj = {
+    senderName: auth.currentUser.displayName,
+    senderEmail: auth.currentUser.email,
+    senderUid: auth.currentUser.uid,
+    senderPic: auth.currentUser.photoURL||'',
+    reciverName: friends.Name,
+    reciverEmail: friends.Email,
+    reciverUid: friends.Uid,
+    reciverPic: friends.ProfilePic,
+    msg: massage||'',
+    img: imgdownloadURL||'',
+    createdAt: getTime(),
+  }
 }
 
   return (
@@ -57,6 +124,7 @@ const handleImage = (event)=>{
               <BsThreeDotsVertical />
             </span>
           </div>
+            {/* chat section start */}
           <div className="w-full h-[600px] py-[47px] px-[65px] flex flex-col gap-y-[25px] overflow-y-scroll">
             <div className="self-start w-[60%]">
               <p className="font-poppins text-[16px] font-medium px-[52px] py-[13px] bg-[#F1F1F1] rounded-[10px] inline-block max-w-[60%] my-[7px] msgIcon">
@@ -75,6 +143,7 @@ const handleImage = (event)=>{
               </p>
             </div>
           </div>
+            {/* chat section start */}
           <div>
             <div className="w-full relative flex gap-x-[20px] px-5 py-[35px] border-t">
               <input
@@ -102,7 +171,7 @@ const handleImage = (event)=>{
                   <IoCameraOutline />
                 </span>
               </div>
-              <button className="p-[15px] bg-primary_Color text-white text-2xl rounded-[10px]">
+              <button className="p-[15px] bg-primary_Color text-white text-2xl rounded-[10px]" onClick={handleSendMsg}>
                 <RiSendPlaneFill />
               </button>
             </div>
@@ -130,17 +199,41 @@ const handleImage = (event)=>{
                     />
                   </svg>
                   <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
-                    <span className="font-semibold">Click to upload</span> or drag
-                    and drop
+                    <span className="font-semibold">Click to upload</span> or
+                    drag and drop
                   </p>
                   <p className="text-xs text-gray-500 dark:text-gray-400">
                     SVG, PNG, JPG or GIF (MAX. 800x400px)
                   </p>
                 </div>
-                <input id="dropzone-file" type="file" className="hidden" accept="image/*" onChange={handleImage}/>
+                <input
+                  id="dropzone-file"
+                  type="file"
+                  className="hidden"
+                  accept="image/*"
+                  onChange={handleImage}
+                />
               </label>
             </div>
-            <button className="w-full border mt-5 py-3 rounded-lg bg-primary_Color font-poppins text-white font-semibold">Sent</button>
+            {img && (
+              progress ? (
+                <div className="w-full mt-5 rounded-lg bg-gray-200 ">
+                  <button
+                    className="py-3 rounded-lg bg-primary_Color font-poppins text-white font-semibold"
+                    style={{ width: `${progress}%` }} // Use object for style
+                  >
+                    {`${progress}%`}
+                  </button>
+                </div>
+              ) : (
+                <button
+                  className="w-full mt-5 py-3 rounded-lg bg-primary_Color font-poppins text-white font-semibold"
+                  onClick={handleSentImg}
+                >
+                  Sent
+                </button>
+              )
+            )}
           </ModalComponent>
         </>
       )}
